0c17e2462dcba8107231d266d3dd5adc672bf1b4
johnymonster <jnelson11@gmail.com>
2012-08-18 13:09:04 -0400

    Updated to use simple els template

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/',function(req,res){
-	res.send('hello');
+	res.render('index');
 });
 
 app.listen(3000);
